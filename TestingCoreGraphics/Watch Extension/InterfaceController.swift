d9d54a7bc36233b876be1267785c5435d8789481
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-07-31 22:09:47 -0500

    Add timer to watch target with coregraphics

--- TestingCoreGraphics/Watch Extension/InterfaceController.swift
+++ TestingCoreGraphics/Watch Extension/InterfaceController.swift
@@ -9,13 +9,62 @@
 import WatchKit
 import Foundation
 
-
+@IBDesignable
 class InterfaceController: WKInterfaceController {
+
+    var counter: Double = 30.0
+    @IBOutlet var timerImage: WKInterfaceImage!
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
+        NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(self.updateTimer), userInfo: nil, repeats: true)
+
+    }
+
+    func configureTimerWithCounter(counter: Double) {
+
+        let size = CGSizeMake(self.contentFrame.width, 6)
+        UIGraphicsBeginImageContext(size)
+        let context = UIGraphicsGetCurrentContext()
+        UIGraphicsPushContext(context!)
+
+        // Draw line
+        let path = UIBezierPath()
+        path.lineWidth = 100
+
+        path.moveToPoint(CGPoint(x: 0, y: 0))
+
+        let counterPosition = (self.contentFrame.width/30)*CGFloat(counter)
+        path.addLineToPoint(CGPoint(x: counterPosition, y: 0))
+
+        UIColor.greenColor().setStroke()
+        UIColor.whiteColor().setFill()
+        path.stroke()
+        path.fill()
+
+        // Convert to UIImage
+        let cgimage = CGBitmapContextCreateImage(context);
+        let uiimage = UIImage(CGImage: cgimage!)
+
+        // End the graphics context
+        UIGraphicsPopContext()
+        UIGraphicsEndImageContext()
+
+        self.timerImage.setImage(uiimage)
+
+    }
+
+    func updateTimer() {
+
+        counter -= 0.1
+
+        if self.counter <= 0 {
+            self.counter = 30
+        }
+
+        configureTimerWithCounter(counter)
     }
 
     override func willActivate() {
