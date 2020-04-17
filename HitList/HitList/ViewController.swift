6226686a8c67e0814ea37ce9dd9698e97562a10f
Adriana Pineda <apineda@twilio.com>
2020-04-17 08:13:53 +0300

    Add table use and use core data to store people

--- HitList/HitList/ViewController.swift
+++ HitList/HitList/ViewController.swift
@@ -7,14 +7,100 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+
+    var people: [NSManagedObject] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "The List"
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "Cell")
     }
 
+    @IBAction func add(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Name",
+                                      message: "Add a new name",
+                                      preferredStyle: .alert)
+
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) {
+                                        [unowned self] action in
+
+                                        guard let textField = alert.textFields?.first,
+                                            let nameToSave = textField.text else {
+                                                return
+                                        }
+
+                                        self.save(name: nameToSave)
+                                        self.tableView.reloadData()
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+
+        alert.addTextField()
+
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+
+        present(alert, animated: true)
+    }
+
+    func save(name: String) {
+
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+
+        // 2
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Person",
+                                       in: managedContext)!
+
+        let person = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+
+        // 3
+        person.setValue(name, forKeyPath: "name")
+
+        // 4
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
 
 }
 
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell {
+
+            let cell =
+                tableView.dequeueReusableCell(withIdentifier: "Cell",
+                                              for: indexPath)
+            let person = people[indexPath.row]
+            cell.textLabel?.text = person.value(forKeyPath: "name") as? String
+            return cell
+    }
+}
