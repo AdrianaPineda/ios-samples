7665e1fa2b6125d365a3c77f626a18a2835b3993
Adriana Pineda <apineda@twilio.com>
2020-04-23 06:40:40 +0300

    Fetch data from coredata

--- HitList/HitList/ViewController.swift
+++ HitList/HitList/ViewController.swift
@@ -22,6 +22,31 @@ class ViewController: UIViewController {
                            forCellReuseIdentifier: "Cell")
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        //1
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+
+        //2
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Person")
+
+        //3
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+
+
     @IBAction func add(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "New Name",
                                       message: "Add a new name",
