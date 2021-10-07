5d643d7dc08a4054d1fbf3d511828e5694b7571f
AdrianaPineda <am.pineda206@gmail.com>
2021-10-06 22:20:58 -0400

    Add new swiftui project

---
+++ swiftui/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Adriana Pineda on 10/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
